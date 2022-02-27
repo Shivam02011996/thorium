6f5e0cef106900bce47fe9c3bd4332a0e94af8a3
Shivam Mishra <shivammishra0002@gmail.com>
2022-02-27 17:59:59 +0530

    dont get

--- src/routes/route.js
+++ src/routes/route.js
@@ -44,6 +44,17 @@ router.post("/post-query-2", function (req, res) {
 })
 
 
+
+
+
+
+
+
+
+
+
+
+
 // ASSIGNMENT:
 // you will be given an array of persons ( i.e an array of objects )..each person will have  a {name: String , age: Number, votingStatus: true/false(Boolean)}
 // take input in query param as votingAge..and for all the people above that age, change votingStatus as true
@@ -79,6 +90,52 @@ router.post("/post-query-2", function (req, res) {
 // ]
 
 
+
+
+let persons = [
+    {
+        name: "PK",
+        age: 10,
+        votingStatus: false
+    },
+    {
+        name: "SK",
+        age: 20,
+        votingStatus: false
+    },
+    {
+        name: "AA",
+        age: 70,
+        votingStatus: false
+    },
+    {
+        name: "SC",
+        age: 5,
+        votingStatus: false
+    },
+    {
+        name: "HO",
+        age: 40,
+        votingStatus: false
+    }
+]
+
+let personCanVote=[];
+
+router.post("/post", function (req, res) {
+    let input = req.query.input
+
+    for (let i = 0; i < persons.length; i++) {
+
+        let ages = persons[i].age 
+        
+           if(input <- ages) {persons[i].votingStatus = true
+            personCanVote.push(persons[i].name) }
+        
+    }
+
+     res.send({persons , personCanVote})
+})
 
 
 
